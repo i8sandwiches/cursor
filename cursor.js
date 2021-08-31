5ec4e2fc60c3324ebb430c8913d431e6c993c7ff
i8sandwiches <61651772+i8sandwiches@users.noreply.github.com>
2021-09-01 06:34:50 +0900

    add another model = same design

--- cursor.js
+++ cursor.js
@@ -1,24 +1,46 @@
 const mosueCursor = document.querySelector(".cursor");
-const containerLink = document.querySelectorAll(".container ul");
+const beBigCursor = document.querySelector(".be__big-cursor");
 
-window.addEventListener('mousemove',cursor)
+const BE_BIG_CURSOR = "be__big-cursor";
+const GROW_CURSOR = "grow-cursor";
+const HOVERED_CURSOR = "hovered-cursor";
 
-function cursor(e/*e=event*/ ) {
+
+window.addEventListener("mousemove", cursor);
+window.addEventListener("mouseover", activeCursor);
+window.addEventListener("mouseleave", nonActiveCursor);
+beBigCursor.addEventListener("mouseleave",nonActiveCursor);
+
+
+function cursor(e) {
     mosueCursor.style.left = e.pageX + "px";
     mosueCursor.style.top = e.pageY + "px";
     mosueCursor.style.display = "block"
 }
-/* study about a 'forEach'*/
-containerLink.forEach(link => {
-    link.addEventListener("mouseover", ()=> {
-        mosueCursor.classList.add('link-grow');
-        link.classList.add("hovered-link");
-    })
-    link.addEventListener("mouseleave", ()=> {
-        mosueCursor.classList.remove('link-grow');
-        link.classList.remove("hovered-link");
-    });
-}); 
-/*----------------------*/
+function activeCursor(e) {
+    const item = e.target;
+    if(item.classList.contains(BE_BIG_CURSOR)){
+        mosueCursor.classList.add(GROW_CURSOR);
+        item.classList.add(HOVERED_CURSOR);
+    } else {
+        mosueCursor.classList.remove(GROW_CURSOR);
+    }    
+}
+function nonActiveCursor() {
+    beBigCursor.classList.remove(HOVERED_CURSOR);
+}
 
-/*get info from 'Dev Ed__youtuber'*/
+//__model_2__//////////////////////////////////////////////////////
+//BeBigCursor.forEach(link => {
+//    link.addEventListener("mouseover", ()=> {
+//        mosueCursor.classList.add("grow-cursor");
+//        link.classList.add("hovered-cursor");
+//    });
+//    link.addEventListener("mouseleave", ()=> {
+//        mosueCursor.classList.remove("grow-cursor");
+//       link.classList.remove("hovered-cursor");
+//    });
+//}); 
+
+//get info from 'Dev Ed__youtuber'
+///////////////////////////////////////////////////////////////////
